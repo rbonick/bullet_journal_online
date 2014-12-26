4dee77fd90553c438a3925a47f8c93a174f4c4e6
Ryan <rjbman93@gmail.com>
2014-12-25 21:14:31 -0600

    Added link for next seven days and set it to be the homepage

--- entries/urls.py
+++ entries/urls.py
@@ -6,5 +6,5 @@ from entries import views
 
 urlpatterns = patterns('',
     url(r'^(?P<year>\d+)/(?P<month>\d+)/$', views.view_month_entries, name="entries"),
-    url(r'^$', views.view_month_entries, name="home"),
+    url(r'^next-seven/$', views.view_next_seven_days_entries, name="home"),
 )
--- entries/views.py
+++ entries/views.py
@@ -1,7 +1,7 @@
 from datetime import date
 from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404
-from dateutil.utils import get_dates
+from dateutil.utils import get_dates, get_next_seven_days
 
 from entries.utils import get_all_entries
 
@@ -12,9 +12,20 @@ def view_month_entries(request, month=date.today().month, year=date.today().year
     entries = {}
     month = int(month)
     year = int(year)
-    for date in get_dates(month, year):
-        entries[date] = get_all_entries(user, date)
+    for day in get_dates(month, year):
+        entries[day] = get_all_entries(user, day)
     return render(request, 'entries.html', {
         "entries": entries,
         "current_date": date.today(),
+    })
+
+
+def view_next_seven_days_entries(request):
+    user = get_object_or_404(User, username="rbonick")
+
+    entries = {}
+    for day in get_next_seven_days():
+        entries[day] = get_all_entries(user, day)
+    return render(request, 'entries.html', {
+        "entries": entries,
     })

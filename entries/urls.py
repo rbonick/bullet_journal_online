6feaac353dc265a672c586e51e87c666c2a5b3b5
Ryan <rjbman93@gmail.com>
2014-12-27 18:58:03 -0600

    Modularized the css for the entry page

--- entries/urls.py
+++ entries/urls.py
@@ -5,9 +5,14 @@ from entries import views
 
 
 urlpatterns = patterns('',
+    # Web pages
     url(r'^(?P<year>\d+)/(?P<month>\d+)/$', views.view_month_entries, name="entries"),
     url(r'^next-seven/$', views.view_next_seven_days_entries, name="home"),
     url(r'^archive/$', views.view_archive, name="view_archive"),
+
+    # Form urls
     url(r'^create/$', views.create_entry, name="create_entry"),
+
+    # Ajax urls
     url(r'^toggle-todo/(?P<todo_id>\d+)/$', views.toggle_todo, name="toggle_todo"),
 )

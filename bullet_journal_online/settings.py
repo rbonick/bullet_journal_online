b97b5b7c77d6fdd3609d7192a12635353d3cff8a
Ryan <rjbman93@gmail.com>
2015-01-14 22:48:20 -0600

    Still fiddling with settings for heroku

--- bullet_journal_online/settings.py
+++ bullet_journal_online/settings.py
@@ -126,6 +126,7 @@ SESSION_SERIALIZER = "django.contrib.sessions.serializers.PickleSerializer"
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
+DATABASES = {}
 DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
